cb0930a7a079ca127c4227ef38754abf36d0170c
Steven <sfischer@redventures.com>
2017-01-19 11:23:20 -0500

    stable animations and collision;

--- background.js
+++ background.js
@@ -27,19 +27,19 @@ var postition = 0,
 stage.interactive = true;
 
 function carStartPos() {
-  scfImg.x = 6;
-  scfImg.y = 200;
-  scfImg.vx = 0;
-  scfImg.vy = 0;
-  scfImg.scale.x = 0.5;
-  scfImg.scale.y = 0.5;
+    scfImg.x = 6;
+    scfImg.y = 200;
+    scfImg.vx = 0;
+    scfImg.vy = 0;
+    scfImg.scale.x = 0.5;
+    scfImg.scale.y = 0.5;
 
-  redCar.x = 600;
-  redCar.y = 200;
-  redCar.vx = 0;
-  redCar.vy = 0;
-  redCar.scale.x = 0.5;
-  redCar.scale.y = 0.5;
+    redCar.x = 600;
+    redCar.y = 200;
+    redCar.vx = 0;
+    redCar.vy = 0;
+    redCar.scale.x = 0.5;
+    redCar.scale.y = 0.5;
 }
 
 function onAssetsLoaded(loader, res) {
@@ -57,9 +57,6 @@ function onAssetsLoaded(loader, res) {
 
 
     var scale = 0.3;
-
-    stage.on('mousedown', onTouchStart);
-    stage.on('touchstart', onTouchStart);
 
     //Capture the keyboard arrow keys
     var left = keyboard(37),
@@ -109,21 +106,22 @@ function onAssetsLoaded(loader, res) {
         }
     };
 
-    function onTouchStart() {
+    //Create the text sprite
+    message = new PIXI.Text(
+        "No collision...", {
+            font: "18px sans-serif",
+            fill: "white"
+        }
+    );
+    message.position.set(8, 8);
+    stage.addChild(message);
 
-    }
     state = play;
     animate();
 }
 
 function animate() {
-
-    //postition += 0; // disable animation
-
-
-
-
-postition += 10;
+    postition += 10;
 
     function bgAnim(sprite) {
 
@@ -148,6 +146,19 @@ postition += 10;
         redCar.position.x += 1286 * 2;
     }
     redCar.position.x -= 1286;
+
+    //check for a collision between the cat and the box
+    if (hitTestRectangle(scfImg, redCar)) {
+      //if there's a collision, change the message text
+      //and tint the box red
+      message.text = "hit!";
+
+    } else {
+      //if there's no collision, reset the message
+      //text and the box's color
+      message.text = "No collision...";
+
+    }
 
 
     bgAnim(background);
@@ -204,4 +215,79 @@ function keyboard(keyCode) {
         "keyup", key.upHandler.bind(key), false
     );
     return key;
+}
+
+//The `hitTestRectangle` function
+function hitTestRectangle(r1, r2) {
+  //Define the variables we'll need to calculate
+  var hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+  //hit will determine whether there's a collision
+  hit = false;
+  //Find the center points of each sprite
+  r1.centerX = r1.x + r1.width / 2;
+  r1.centerY = r1.y + r1.height / 2;
+  r2.centerX = r2.x + r2.width / 2;
+  r2.centerY = r2.y + r2.height / 2;
+  //Find the half-widths and half-heights of each sprite
+  r1.halfWidth = r1.width / 2;
+  r1.halfHeight = r1.height / 2;
+  r2.halfWidth = r2.width / 2;
+  r2.halfHeight = r2.height / 2;
+  //Calculate the distance vector between the sprites
+  vx = r1.centerX - r2.centerX;
+  vy = r1.centerY - r2.centerY;
+  //Figure out the combined half-widths and half-heights
+  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+  //Check for a collision on the x axis
+  if (Math.abs(vx) < combinedHalfWidths) {
+    //A collision might be occuring. Check for a collision on the y axis
+    if (Math.abs(vy) < combinedHalfHeights) {
+      //There's definitely a collision happening
+      hit = true;
+    } else {
+      //There's no collision on the y axis
+      hit = false;
+    }
+  } else {
+    //There's no collision on the x axis
+    hit = false;
+  }
+  //`hit` will be either `true` or `false`
+  return hit;
+};
+//The `keyboard` helper function
+function keyboard(keyCode) {
+  var key = {};
+  key.code = keyCode;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+  //The `downHandler`
+  key.downHandler = function(event) {
+    if (event.keyCode === key.code) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+    }
+    event.preventDefault();
+  };
+  //The `upHandler`
+  key.upHandler = function(event) {
+    if (event.keyCode === key.code) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+    }
+    event.preventDefault();
+  };
+  //Attach event listeners
+  window.addEventListener(
+    "keydown", key.downHandler.bind(key), false
+  );
+  window.addEventListener(
+    "keyup", key.upHandler.bind(key), false
+  );
+  return key;
 }
